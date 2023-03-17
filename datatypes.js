171943cfb76751ebed64a743e6b3e38f39dc8791
SachinSamuel-CSBS <107710112+SachinSamuel-CSBS@users.noreply.github.com>
2023-03-17 12:49:57 +0530

    Update datatypes.js

--- datatypes.js
+++ datatypes.js
@@ -1,1 +1,19 @@
+var n=10;
+var str="Hello";
+var flag=true;
+var p;
+var abc=null;
+document.write(typeof(n)," ");
+document.write(typeof(str)," ");
+document.write(typeof(flag)," ");
+document.write(typeof(p)," ");
+document.write(typeof(abc)," ");
 
+//Type Converstion
+
+var num=String(10);
+document.write(typeof(num)," ");
+var bool=String(true);
+document.write(typeof(bool)," ");
+var str=Number("10");
+document.write(typeof(str)," ");
