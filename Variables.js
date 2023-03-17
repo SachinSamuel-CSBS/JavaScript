b6fdad7fb6852b58e14a540b612ca3cf037e958d
SachinSamuel-CSBS <107710112+SachinSamuel-CSBS@users.noreply.github.com>
2023-03-17 12:47:45 +0530

    Update Variables.js

--- Variables.js
+++ Variables.js
@@ -1,1 +1,20 @@
+document.write("Hello World!<br>");
+document.write(6+4,"<br>");
+document.write(6*4,"<br>");
+document.write(3/2,"<br>");
 
+console.log("hello console");
+
+
+//variables
+
+var a=7;
+var b=5;
+var c=a*b;
+document.write("The value of c is ",c,"<br>");
+
+var bikename;
+document.write(bikename); //default value is undefind;
+
+bikename="yamaga";
+document.write("<br>",bikename);
