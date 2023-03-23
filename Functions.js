3cf81bdbcfe3e7c20b7a4b63c4fa4056a5d2c96c
SachinSamuel-CSBS <107710112+SachinSamuel-CSBS@users.noreply.github.com>
2023-03-23 23:45:35 +0530

    Update Functions.js

--- Functions.js
+++ Functions.js
@@ -1,1 +1,38 @@
+function sum1()
+{
+    var x=10,y=5;
+    var sum=x+y;
+    document.write(sum);
+}
+sum1();
 
+document.write("<br>");
+
+//passing the parameter
+function sum2(x,y)
+{
+    var sum=x+y;
+    document.write(sum);
+}
+sum2(10,20);
+
+document.write("<br>");
+
+//passing string as parameter
+function sum3(x,y)
+{
+    var sum=x+y;
+    document.write(sum);
+}
+sum2("Welcome to ","JavaScript");
+
+document.write("<br>");
+
+//with return value
+function sum4(x,y)
+{
+    var sum=x+y;
+    return sum;
+}
+var output=sum4(5,9);
+document.write(output);
