d71c135edf6a90e2e2e2bcb20365f3ed6340c75f
SachinSamuel-CSBS <107710112+SachinSamuel-CSBS@users.noreply.github.com>
2023-03-23 23:48:18 +0530

    Update Scope of variables.js

--- Scope of variables.js
+++ Scope of variables.js
@@ -1,1 +1,31 @@
+var a=5;
+document.write(a,"<br>");
 
+
+
+
+document.write(a,"<br>");
+
+function fun1()
+{
+    var x=10;
+    document.write(x,"<br>");
+}
+fun1();
+
+
+function fun2()
+{
+    var b=10;
+}
+fun2();
+document.write(b,"<br>");
+
+var y=15;
+function fun3()
+{
+    var z=40;
+    document.write(y,"<br>");
+    document.write(z,"<br>");
+}
+fun3();
